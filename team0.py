010983aa8310e5c3118bc49959661bfe33b5258e
Daniel Lande <danlande@gmail.com>
2017-01-04 08:29:15 -0700

    Added code for team0 strategy

--- team0.py
+++ team0.py
@@ -5,10 +5,11 @@
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
+import random
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Mr. Lande' # Only 10 chars displayed.
+strategy_name = 'Randomness....'
+strategy_description = 'Betray 66% of the time, collude the rest'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,7 +27,10 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    if random.randint(1,3) % 2 == 0:
+        return 'c'
+    else:
+        return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
