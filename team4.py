66b488393a13df3ce4a4d90cde14d0679a1fc0c3
Brandon Bierman <bjbier2817@student.mcpsmt.org>
2017-01-04 08:37:54 -0700

    Update team4.py
    Added indents.

--- team4.py
+++ team4.py
@@ -11,15 +11,15 @@ strategy_name = 'Payback.'
 strategy_description = 'Betray if they have betrayed 3 or more times.'
 
 def move(my_history, their_history, my_score, their_score):
-if len(my_history)==0: # It's the first round; collude.
-return 'c'
-elif len(their_history) < 180: #If its the last or second to last turn; betray.
-if len(their_history) > 6:
-if 'b' not in their_history[:7]:
-return 'c'
-elif their_history[3] <= 'b' : #If they have betrayed 3 or more times; betray.
-return 'b'
-else: #If all else fails; collude.
-return 'c'
+ if len(my_history)==0: # It's the first round; collude.
+  return 'c'
+ elif len(their_history) < 180: #If its the last or second to last turn; betray.
+  if len(their_history) > 6:
+   if 'b' not in their_history[:7]:
+    return 'c'
+ elif their_history[3] <= 'b' : #If they have betrayed 3 or more times; betray.
+  return 'b'
+ else: #If all else fails; collude.
+  return 'c'
 
       
